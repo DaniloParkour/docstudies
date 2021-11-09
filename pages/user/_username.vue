403a087798010a16bce2409b30f5e8449d4ded86
Danilo Carvalho <danilomlcarvalho@gmail.com>
2021-11-09 16:39:52 -0300

    document & window usage

--- pages/user/_username.vue
+++ pages/user/_username.vue
@@ -12,8 +12,9 @@ export default Vue.extend({
 
   // The asyncData hook and fetch hook should return a promise using return or implicity with async/await
   async asyncData({ params, $axios, error }) {
+    console.log('Runing in ' + (process.client  ? 'client' : 'server'))
     await $axios.get(`https://api.github.com/users/${params.username}`).then(response => {
-      console.log(response)
+      // console.log(response)
     }).catch((e) => console.log(e))
   },
   data () {
@@ -25,6 +26,15 @@ export default Vue.extend({
     return {
       title: 'User Details'
     }
+  },
+  // In beforeMount and in mounted we have access to window that normal window and
+  // document access is not directly because vue/nuxt use a node server
+  beforeMount () {
+    console.log('hello beforeMount. Base URI from document is ' + document.baseURI)
+  },
+  mounted () {
+    console.log('hello mounted. Window is a ' + window)
+    if(pro)
   },
   methods: {
     clickHandler () {
